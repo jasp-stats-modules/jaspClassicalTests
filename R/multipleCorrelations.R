0f625ea0e1f205c99c707c985c4e00287d0ae905
julianwuth <julian.wuth@yahoo.de>
2026-09-14 13:05:32 +0200

    Removing dependency check in R file

--- R/multipleCorrelations.R
+++ R/multipleCorrelations.R
@@ -27,12 +27,6 @@
 #' @import jaspRegression
 #' @export
 multipleCorrelations <- function(jaspResults, dataset, options, ...) {
-  if (!requireNamespace("jaspRegression", quietly = TRUE)) {
-    errorTable <- createJaspTable(title = gettext("Correlation Matrix"))
-    errorTable$setError(gettext("This analysis requires the Regression (jaspRegression) module, which is not available."))
-    jaspResults[["error"]] <- errorTable
-    return()
-  }
 
   jaspRegression::CorrelationInternal(jaspResults, dataset, options)
 }
